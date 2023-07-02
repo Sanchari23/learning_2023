549fbd47b05ecb30ae5efe34459d698659a34bc6
Sanchari23 <85665004+Sanchari23@users.noreply.github.com>
2023-07-03 00:19:20 +0530

    Add files via upload

---
+++ June 16/rotate.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+char rotatestring(char str[], int s){
+    int i;
+    char temp = '\0'; 
+    int length = strlen(str);
+    for(i=0; i<s; i++){
+        temp = str[i];
+        str[i] = 
+    }
+}
